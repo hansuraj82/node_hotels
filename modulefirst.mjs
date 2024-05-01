2ee8002590dc80f659d790904977458e00122440
Hansu <chhotukumar829441@gmail.com>
2024-05-01 14:00:16 +0530

    Every Thing is good

--- modulefirst.mjs
+++
@@ -1,3 +0,0 @@
-export  function simple() {
-    console.log("i am in modulefirst.mjs and in a function named as simple()");
-}
--- pathModule.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
- const b = path.basename('/foo/bar/baz/asdf/quux.html', '.html');
- console.log(b);
- const c = path.extname(__filename);
- console.log(__filename, c);
